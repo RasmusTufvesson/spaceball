9bff3cbb35e5c0c0c981797e1f6c20f4d5742112
RasmusTufvesson <lars@bizzbook.com>
2023-06-02 23:49:49 +0200

    do stuff

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,12 @@ use glam::{
 };
 use opengl_graphics::{OpenGL};
 use rand::{self, Rng, distributions::Standard, prelude::Distribution};
-use imageproc::{drawing::{draw_hollow_circle_mut, draw_hollow_rect_mut}, rect::Rect};
+use imageproc::{drawing::{draw_polygon_mut, draw_filled_rect_mut, draw_filled_circle_mut}, rect::Rect, point::Point};
 
 const BLACK: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
 const WHITE_IM: im::Rgba<u8> = im::Rgba([255, 255, 255, 255]);
 const BLACK_IM: im::Rgba<u8> = im::Rgba([0, 0, 0, 255]);
+const TRIANGLE: [DVec2; 3] = [DVec2 { x: -0.5, y: -0.5 }, DVec2 { x: 0.5, y: -0.5 }, DVec2 { x: 0.0, y: 0.36602540378443864676372317075294 }];
 
 enum ShapeType {
     Circle,
@@ -21,8 +22,9 @@ enum ShapeType {
 
 impl Distribution<ShapeType> for Standard {
     fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> ShapeType {
-        match rng.gen_range(0..=1) {
+        match rng.gen_range(0..=2) {
             0 => ShapeType::Circle,
+            1 => ShapeType::Triangle,
             _ => ShapeType::Square,
         }
     }
@@ -33,6 +35,40 @@ struct Shape {
     pub shape_type: ShapeType,
     pub speed: DVec2,
     pub radius: f64,
+    pub image: im::ImageBuffer<im::Rgba<u8>, Vec<u8>>,
+}
+
+fn draw_shape(shape_type: &ShapeType, radius: f64) -> im::ImageBuffer<im::Rgba<u8>, Vec<u8>> {
+    let mut img: im::ImageBuffer<im::Rgba<u8>, Vec<_>> = im::ImageBuffer::new((radius * 2.0).round() as u32, (radius * 2.0).round() as u32);
+    match shape_type {
+        ShapeType::Circle => {
+            draw_filled_circle_mut(&mut img, (
+                radius.round() as i32, radius.round() as i32
+            ), radius.round() as i32, WHITE_IM);
+        }
+        ShapeType::Square => {
+            draw_filled_rect_mut(&mut img,
+                Rect::at(
+                    0,
+                    0,
+                ).of_size(
+                    (radius * 2.0).round() as u32,
+                    (radius * 2.0).round() as u32,
+                )
+            .into(), WHITE_IM)
+        }
+        ShapeType::Triangle => {
+            let world_pos = DVec2::new(radius, radius);
+            let triangle = [
+                to_point(from_relative(TRIANGLE[0] * radius, world_pos)),
+                to_point(from_relative(TRIANGLE[1] * radius, world_pos)),
+                to_point(from_relative(TRIANGLE[2] * radius, world_pos)),
+            ];
+            draw_polygon_mut(&mut img, &triangle, WHITE_IM);
+        }
+        _ => {}
+    }
+    return img;
 }
 
 struct World {
@@ -66,8 +102,10 @@ impl App {
                 self.world.shapes[n].position.y = -self.half_size.y - radius
             }
             self.world.shapes[n].radius -= 0.1 * args.dt;
-            if self.world.shapes[n].radius.round() <= 0.0 {
+            if self.world.shapes[n].radius.round() <= 1.0 {
                 to_remove.push(n);
+            } else if self.world.shapes[n].radius.round() != radius.round() {
+                self.world.shapes[n].image = draw_shape(&self.world.shapes[n].shape_type, self.world.shapes[n].radius);
             }
         }
         for index in to_remove.into_iter().rev() {
@@ -83,7 +121,8 @@ impl App {
                     let speed = DVec2::new(self.rng.gen::<f64>() * 450.0 - 225.0, self.rng.gen::<f64>() * 450.0 - 225.0);
                     let radius = self.rng.gen::<f64>() * 17.0 + 3.0;
                     let shape_type = self.rng.gen::<ShapeType>();
-                    let shape = Shape { position, shape_type, speed, radius };
+                    let image = draw_shape(&shape_type, radius);
+                    let shape = Shape { position, shape_type, speed, radius, image };
                     self.world.shapes.push(shape);
                 }
             }
@@ -131,25 +170,11 @@ fn main() {
                     *p = BLACK_IM;
                 }
                 for shape in &app.world.shapes {
-                    match shape.shape_type {
-                        ShapeType::Circle => {
-                            draw_hollow_circle_mut(&mut canvas, (
-                                (half_size.x + shape.position.x).round() as i32, (half_size.y - shape.position.y).round() as i32
-                            ), shape.radius.round() as i32, WHITE_IM);
-                        }
-                        ShapeType::Square => {
-                            draw_hollow_rect_mut(&mut canvas,
-                                Rect::at(
-                                    (half_size.x + shape.position.x - shape.radius).round() as i32,
-                                    (half_size.y - shape.position.y - shape.radius).round() as i32,
-                                ).of_size(
-                                    (shape.radius * 2.0).round() as u32,
-                                    (shape.radius * 2.0).round() as u32,
-                                )
-                            .into(), WHITE_IM)
-                        }
-                        _ => {}
-                    }
+                    let coords = [
+                        (half_size.x + shape.position.x - shape.radius).round() as i64,
+                        (half_size.y - shape.position.y - shape.radius).round() as i64,
+                    ];
+                    im::imageops::overlay(&mut canvas, &shape.image, coords[0], coords[1]);
                 }
                 texture.update(&mut texture_context, &canvas).unwrap();
                 texture_context.encoder.flush(device);
@@ -165,4 +190,12 @@ fn main() {
             app.keydown(&button);
         }
     }
+}
+
+fn to_point(vec: DVec2) -> Point<i32> {
+    Point { x: vec.x.round() as i32, y: vec.y.round() as i32 }
+}
+
+fn from_relative(vec: DVec2, position: DVec2) -> DVec2 {
+    DVec2 { x: position.x + vec.x, y: position.y + vec.y }
 }
